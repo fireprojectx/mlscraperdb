21a5e805aac74c9aa211d6043c2c146ed3225afd
Wallace <167040540+fireprojectx@users.noreply.github.com>
2025-06-14 23:00:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
 from scraper import extrair_dados_produto
 import os
-
+from fastapi.responses import JSONResponse
+from db import get_connection
 
 app = FastAPI()
 
@@ -17,6 +18,7 @@ def scrape_product(url: str):
 
 # Serve frontend
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
+
 
 
 # Inserção no banco após scraping
@@ -41,3 +43,27 @@ def salvar_no_banco(dados, url):
     conn.commit()
     cur.close()
     conn.close()
+
+
+@app.get("/historico")
+def obter_historico():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT titulo, preco, vendidos, data_inicio, data_consulta, url
+            FROM consultas
+            ORDER BY data_consulta DESC
+        """)
+        rows = cur.fetchall()
+
+        colunas = [desc[0] for desc in cur.description]
+        dados = [dict(zip(colunas, linha)) for linha in rows]
+
+        cur.close()
+        conn.close()
+
+        return JSONResponse(content=dados)
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"erro": str(e)})
